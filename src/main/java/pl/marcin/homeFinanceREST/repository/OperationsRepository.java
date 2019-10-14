5707cef7aca37e8aa5819d98012c790c733680fd
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-14 23:32:32 +0200

    changed type and modified method

--- src/main/java/pl/marcin/homeFinanceREST/repository/OperationsRepository.java
+++ src/main/java/pl/marcin/homeFinanceREST/repository/OperationsRepository.java
@@ -4,13 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import pl.marcin.homeFinanceREST.entity.Operation;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface OperationsRepository extends JpaRepository<Operation, Long> {
 
     @Query("SELECT o FROM Operation o WHERE o.orderDate = ?1 AND o.description = ?2 AND o.amount = ?3")
-    Operation findOperationByOrderDateDescriptionAmount(String orderDate, String description, Double amount);
+    Operation findOperationByOrderDateDescriptionAmount(LocalDate orderDate, String description, Double amount);
 
-    @Query("SELECT o FROM Operation o ORDER BY o.orderDate DESC")
-    List<Operation> findOperationsOrderByOrderDateDesc();
+    @Query("SELECT o FROM Operation o WHERE o.orderDate BETWEEN ?1 AND ?2 ORDER BY o.orderDate DESC")
+    List<Operation> findOperationsByOrderDateOrderByOrderDateDesc(LocalDate fromDate, LocalDate toDate);
 }
