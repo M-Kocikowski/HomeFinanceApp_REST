e1c3162f038aaf04c9f3246ba6646d2105c86d72
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-10-19 23:08:54 +0200

    refactoring

--- src/main/java/pl/marcin/homeFinanceREST/controller/ApiOperationsController.java
+++ src/main/java/pl/marcin/homeFinanceREST/controller/ApiOperationsController.java
@@ -3,8 +3,7 @@ package pl.marcin.homeFinanceREST.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.marcin.homeFinanceREST.entity.Operation;
-import pl.marcin.homeFinanceREST.services.DbOperations;
-import pl.marcin.homeFinanceREST.services.XMLToEntity;
+import pl.marcin.homeFinanceREST.services.OperationsServices;
 import pl.marcin.homeFinanceREST.xmlModel.XMLOperation;
 
 import javax.xml.bind.JAXBException;
@@ -12,46 +11,44 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/operations")
 @CrossOrigin(origins = "http://localhost:3000")
-public class ApiController {
+public class ApiOperationsController {
 
-    private XMLToEntity xmlToEntity;
-    private DbOperations dbOperations;
+    private OperationsServices operationsServices;
 
-    public ApiController(XMLToEntity xmlToEntity, DbOperations dbOperations) {
-        this.xmlToEntity = xmlToEntity;
-        this.dbOperations = dbOperations;
+    public ApiOperationsController(OperationsServices operationsServices) {
+        this.operationsServices = operationsServices;
     }
 
     @PostMapping("/post/file")
     public String fileHandler(@RequestParam("file") MultipartFile file) throws JAXBException, IOException {
-        List<XMLOperation> xmlOperations = xmlToEntity.convertXMLToOperations(file);
-        List<Operation> operationsFromXML = xmlToEntity.getOperationsFromXML(xmlOperations);
-        dbOperations.saveOperationsToDatabase(operationsFromXML);
+        List<XMLOperation> xmlOperations = operationsServices.convertXMLToOperations(file);
+        List<Operation> operationsFromXML = operationsServices.getOperationsFromXML(xmlOperations);
+        operationsServices.saveOperationsToDatabase(operationsFromXML);
         return "OK";
     }
 
     @PostMapping("/post")
     public Operation saveSingleOperation(@RequestBody Operation operation){
-        dbOperations.saveSingleOperationToDatabase(operation);
+        operationsServices.saveSingleOperationToDatabase(operation);
         return operation;
     }
 
-    @GetMapping("/operations/{fromDate}/{toDate}")
+    @GetMapping("/{fromDate}/{toDate}")
     public List<Operation> getAllOperations(@PathVariable String fromDate, @PathVariable String toDate){
-        return dbOperations.getOperationsByDate(fromDate, toDate);
+        return operationsServices.getOperationsByDate(fromDate, toDate);
     }
 
     @PutMapping("/put/{id}")
     public Operation updateSingleOperation(@RequestBody Operation operation){
-        dbOperations.saveSingleOperationToDatabase(operation);
+        operationsServices.saveSingleOperationToDatabase(operation);
         return operation;
     }
 
-    @GetMapping("/operation/{id}")
+    @GetMapping("/{id}")
     public Operation getSingleOperationById(@PathVariable long id){
-        return dbOperations.getSingleOperationById(id);
+        return operationsServices.getSingleOperationById(id);
     }
 
 }
---
+++ src/main/java/pl/marcin/homeFinanceREST/services/OperationsServices.java
@@ -0,0 +1,91 @@
+package pl.marcin.homeFinanceREST.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import pl.marcin.homeFinanceREST.entity.Operation;
+import pl.marcin.homeFinanceREST.repository.OperationsRepository;
+import pl.marcin.homeFinanceREST.xmlModel.XMLAccountHistory;
+import pl.marcin.homeFinanceREST.xmlModel.XMLOperation;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OperationsServices {
+
+    private OperationsRepository repository;
+
+    public OperationsServices(OperationsRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<XMLOperation> convertXMLToOperations(MultipartFile file) throws JAXBException, IOException {
+
+        JAXBContext context = JAXBContext.newInstance(XMLAccountHistory.class);
+        XMLAccountHistory XMLAccountHistory = (XMLAccountHistory) context.createUnmarshaller()
+                .unmarshal(file.getInputStream());
+        return XMLAccountHistory.getXMLOperations().getXMLOperationList();
+
+    }
+
+    public List<Operation> getOperationsFromXML(List<XMLOperation> xmlOperations){
+
+        List<Operation> operations = new ArrayList<>();
+        for (XMLOperation o : xmlOperations) {
+
+            Operation operation = new Operation();
+            operation.setType(o.getType());
+            operation.setOrderDate(LocalDate.parse(o.getOrderDate()));
+            operation.setDescription(o.getDescription());
+            operation.setAmount(o.getAmount());
+            operations.add(operation);
+
+        }
+        return operations;
+    }
+
+    public void saveOperationsToDatabase(List<Operation> operations){
+
+        List<Operation> filteredOperations = operations.stream()
+                .filter(this::checkIfOperationNotDuplicated)
+                .collect(Collectors.toList());
+
+        repository.saveAll(filteredOperations);
+
+    }
+
+    public void saveSingleOperationToDatabase(Operation operation){
+        if (checkIfOperationNotDuplicated(operation)){
+            repository.save(operation);
+        }
+    }
+
+    public List<Operation> getOperationsByDate(String fromDate, String toDate){
+
+        return repository.findOperationsByOrderDateOrderByOrderDateDesc(
+                LocalDate.parse(fromDate), LocalDate.parse(toDate)
+        );
+    }
+
+    public Operation getSingleOperationById(long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+
+    private boolean checkIfOperationNotDuplicated(Operation operationToCheck){
+        Operation checkOperationInDatabase = repository.findOperationByOrderDateDescriptionAmount(
+                operationToCheck.getOrderDate(),
+                operationToCheck.getDescription(),
+                operationToCheck.getAmount()
+        );
+        return checkOperationInDatabase == null;
+
+    }
+
+
+}
